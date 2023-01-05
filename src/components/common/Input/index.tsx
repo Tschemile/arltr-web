652b4cce0e20da86b718536e0c17a643412ba977
Dat Nguyen <nguyenphantuandat1998@gmail.com>
2023-01-05 17:25:25 +0700

    UI part 1

---
+++ src/components/common/Input/index.tsx
@@ -0,0 +1,39 @@
+import type { ReactNode } from 'react';
+import React from 'react';
+
+interface InputProps {
+  placeholder?: string;
+  onChange?: (value: string) => void;
+  icons?: ReactNode;
+  width?: string;
+  type?: 'text' | 'password' | 'number';
+  name?: string;
+}
+
+export default function Input(props: InputProps) {
+  const {
+    placeholder = 'Type here...',
+    onChange = () => {},
+    icons = '',
+    width = '100%',
+    type = 'text',
+    name = '',
+  } = props;
+  return (
+    <div className="relative">
+      <div className="absolute top-[50%] left-2 translate-y-[-50%] text-[#929292]">
+        {icons || ''}
+      </div>
+      <input
+        className={`h-[40px] py-2 pr-4 ${
+          icons ? 'pl-10' : 'pl-4'
+        } bg-main rounded-lg outline-none placeholder:text-[#929292]`}
+        placeholder={placeholder}
+        style={{ width }}
+        type={type}
+        name={name}
+        onChange={(e) => onChange(e.target.value)}
+      />
+    </div>
+  );
+}
