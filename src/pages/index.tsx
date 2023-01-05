9c0677c1ca72f4ee672d6685de6d73b1747b888d
Min-Tri <tmtri.290798@gmail.com>
2023-01-05 10:50:38 +0700

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,27 @@
+import { useRouter } from 'next/router';
+
+import { Meta } from '@/layouts/Meta';
+import { Main } from '@/templates/Main';
+
+const Index = () => {
+  const router = useRouter();
+
+  return (
+    <Main
+      meta={
+        <Meta
+          title="mai thuy oi"
+          description="con rong den"
+        />
+      }
+    >
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta commodi enim obcaecati rerum. Voluptate temporibus cumque quisquam necessitatibus neque sed dolorum fuga explicabo. Nihil odit atque, expedita quo voluptas temporibus!
+    Temporibus quod velit accusamus explicabo saepe itaque voluptate tempore ea id at? Veritatis quis repellat harum molestias cumque voluptas soluta maxime et reprehenderit, amet culpa, repellendus, quisquam accusamus tempora fugit!
+    Sunt, laudantium eos, ratione corporis tempore similique eveniet ab temporibus maiores voluptatum quas quasi nesciunt magnam recusandae error nemo modi doloribus sit sed eligendi distinctio, voluptate obcaecati. Voluptatibus, dolorum saepe.
+    Voluptatum dignissimos veniam aut repellat corporis asperiores eaque, distinctio voluptatem voluptate quasi quia deleniti ratione deserunt quibusdam sapiente, eos totam, sed hic provident sequi consequuntur? Officiis ullam quis temporibus blanditiis?
+    Exercitationem laudantium debitis repellat voluptatibus quas tempora dolor mollitia, totam eligendi qui excepturi ex incidunt sapiente tenetur suscipit in nihil hic cumque iure voluptatem magni quisquam ipsum similique autem. Ab.
+    </Main>
+  );
+};
+
+export default Index;
