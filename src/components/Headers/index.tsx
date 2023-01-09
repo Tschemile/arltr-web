ff0ecf68d9cd0e14409716de177b9c15b5201a9b
Dat Nguyen <nguyenphantuandat1998@gmail.com>
2023-01-09 10:49:54 +0700

    Navbar for layout

--- src/components/Headers/index.tsx
+++ src/components/Headers/index.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import IconButton from '../common/IconButton';
 
 import Input from '../common/Input';
+import ArrowDown from '../Icons/ArrowDown';
+import Bell from '../Icons/Bell';
+import Message from '../Icons/Message';
+import PlusIcon from '../Icons/PlusIcon';
 
 export default function Headers() {
   return (
@@ -32,60 +36,16 @@ export default function Headers() {
         />
         <div>
           <IconButton>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="h-7 w-7 p-1"
-            >
-              <path
-                fillRule="evenodd"
-                d="M12 3.75a.75.75 0 01.75.75v6.75h6.75a.75.75 0 010 1.5h-6.75v6.75a.75.75 0 01-1.5 0v-6.75H4.5a.75.75 0 010-1.5h6.75V4.5a.75.75 0 01.75-.75z"
-                clipRule="evenodd"
-              />
-            </svg>
+            <PlusIcon />
           </IconButton>
           <IconButton>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="h-7 w-7 p-1"
-            >
-              <path
-                fillRule="evenodd"
-                d="M4.804 21.644A6.707 6.707 0 006 21.75a6.721 6.721 0 003.583-1.029c.774.182 1.584.279 2.417.279 5.322 0 9.75-3.97 9.75-9 0-5.03-4.428-9-9.75-9s-9.75 3.97-9.75 9c0 2.409 1.025 4.587 2.674 6.192.232.226.277.428.254.543a3.73 3.73 0 01-.814 1.686.75.75 0 00.44 1.223zM8.25 10.875a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25zM10.875 12a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zm4.875-1.125a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25z"
-                clipRule="evenodd"
-              />
-            </svg>
+           <Message />
           </IconButton>
           <IconButton>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="h-7 w-7 p-1"
-            >
-              <path
-                fillRule="evenodd"
-                d="M5.25 9a6.75 6.75 0 0113.5 0v.75c0 2.123.8 4.057 2.118 5.52a.75.75 0 01-.297 1.206c-1.544.57-3.16.99-4.831 1.243a3.75 3.75 0 11-7.48 0 24.585 24.585 0 01-4.831-1.244.75.75 0 01-.298-1.205A8.217 8.217 0 005.25 9.75V9zm4.502 8.9a2.25 2.25 0 104.496 0 25.057 25.057 0 01-4.496 0z"
-                clipRule="evenodd"
-              />
-            </svg>
+            <Bell />
           </IconButton>
           <IconButton>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="h-7 w-7 p-1"
-            >
-              <path
-                fillRule="evenodd"
-                d="M12.53 16.28a.75.75 0 01-1.06 0l-7.5-7.5a.75.75 0 011.06-1.06L12 14.69l6.97-6.97a.75.75 0 111.06 1.06l-7.5 7.5z"
-                clipRule="evenodd"
-              />
-            </svg>
+           <ArrowDown />
           </IconButton>
         </div>
       </div>
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 
 import Avatar from '../common/Avatar';
+import ArrowDown from '../Icons/ArrowDown';
+import ArrowUp from '../Icons/ArrowUp';
 
 const ListMenu = [
   {
@@ -205,21 +207,128 @@ const ListMenu = [
   },
 ];
 
+const listExplore = [
+  {
+    id: 1,
+    title: 'Groups',
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="h-4 w-4"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z"
+        />
+      </svg>
+    ),
+  },
+  {
+    id: 2,
+    title: 'Pages',
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="h-4 w-4"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z"
+        />
+      </svg>
+    ),
+  },
+  {
+    id: 3,
+    title: 'Market',
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="h-4 w-4"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z"
+        />
+      </svg>
+    ),
+  },
+  {
+    id: 4,
+    title: 'Blog',
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="h-4 w-4"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z"
+        />
+      </svg>
+    ),
+  },
+];
+
 export default function Navbar() {
   const [isVisible, setIsVisible] = useState(6);
   return (
     <div className="fixed top-[60px] left-0 h-[calc(100vh-60px)] w-[300px] overflow-y-auto overscroll-contain py-2 px-4">
-      <div className="flex items-center">
+      <button className="nav-item">
         <Avatar
           src="https://i.pinimg.com/474x/ff/0d/f4/ff0df44c4cd43c7cd964e36b4354e56b.jpg"
           alt="avatar"
           width="36px"
         />
         <p className="pl-3">Pé Chouu</p>
-      </div>
+      </button>
       <div className="my-4">
         {ListMenu.slice(0, isVisible).map((x) => (
-          <button className="mb-4 flex w-full items-center" key={x.id}>
+          <button className="nav-item mb-2" key={x.id}>
+            <span className="rounded-full border-2 bg-[#c4c4c4] p-2">
+              {x.icon}
+            </span>
+            <p className="pl-3 text-base font-medium">{x.title}</p>
+          </button>
+        ))}
+        <button
+          className="nav-item"
+          onClick={() => setIsVisible(isVisible <= 6 ? ListMenu.length : 6)}
+        >
+          <span className="rounded-full border-2 bg-[#c4c4c4] p-2">
+            {isVisible <= 6 ? <ArrowDown width={18} /> : <ArrowUp width={18} />}
+          </span>
+
+          <p className="pl-3 text-base font-bold">
+            See {isVisible <= 6 ? 'all' : 'less'}
+          </p>
+        </button>
+      </div>
+      <hr className="my-4 border-[#d6d6d6]" />
+      <h4 className="text-lg font-medium">Explore</h4>
+      <div className="my-4">
+        {listExplore.map((x) => (
+          <button className="nav-item mb-2" key={x.id}>
             <span className="rounded-full border-2 bg-[#c4c4c4] p-2">
               {x.icon}
             </span>
@@ -227,45 +336,20 @@ export default function Navbar() {
           </button>
         ))}
       </div>
-      <button
-        className="flex w-full items-center"
-        onClick={() => setIsVisible(isVisible <= 6 ? ListMenu.length : 6)}
-      >
-        <span className="rounded-full border-2 bg-[#c4c4c4] p-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="h-4 w-4"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d={
-                isVisible <= 6
-                  ? 'M19.5 8.25l-7.5 7.5-7.5-7.5'
-                  : 'M4.5 15.75l7.5-7.5 7.5 7.5'
-              }
-            />
-          </svg>
-        </span>
-
-        <p className="pl-3 text-base font-bold">
-          See {isVisible <= 6 ? 'all' : 'less'}
-        </p>
-      </button>
       <hr className="my-4 border-[#d6d6d6]" />
-      <h4 className="text-lg font-medium">Explore</h4>
-      <div className="text-base">
-        <p>About Us</p>
-        <p>Blog</p>
-        <p>Contact Us</p>
-        <p>Privacy Policy</p>
-        <p>Developers</p>
-        <p>Temp - Conditions</p>
-        <p>© 2020 Fortune. All Rights Reserved.</p>
+
+      <div className="text-sm">
+        <div className="my-2">
+          <p>About Us</p>
+          <p>Blog</p>
+          <p>Contact Us</p>
+          <p>Privacy Policy</p>
+          <p>Developers</p>
+          <p>Temp - Conditions</p>
+        </div>
+        <p>
+          © 2020 <strong>Fortune</strong>. All Rights Reserved.
+        </p>
       </div>
     </div>
   );
